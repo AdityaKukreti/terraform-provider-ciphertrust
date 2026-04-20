3821ee1fbbc94f705ee8e0abdaded85b76931dee
Chinmay <chinmay.kalra@external.thalesgroup.com>
2026-04-20 01:49:54 -0700

    CTE Profile resource changes

---
+++ internal/provider/resource_cte_profile_test.go
@@ -0,0 +1,77 @@
+package provider
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+)
+
+func TestResourceCTEProfile(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+
+			// Step 1: Create
+			{
+				Config: providerConfig + `
+resource "ciphertrust_cte_profile" "profile" {
+  name        = "testProfile1"
+  description = "Initial profile"
+
+  concise_logging = true
+  connect_timeout = 10
+
+  cache_settings = {
+    max_files = 500
+    max_space = 200
+  }
+
+  file_settings = {
+    allow_purge   = true
+    file_threshold = "INFO"
+    max_file_size = 100000
+    max_old_files = 5
+  }
+}
+`,
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttrSet("ciphertrust_cte_profile.profile", "id"),
+				),
+			},
+
+			// Step 2: Update
+			{
+				Config: providerConfig + `
+resource "ciphertrust_cte_profile" "profile" {
+  name        = "testProfile1"
+  description = "Updated profile"
+
+  concise_logging = false
+  connect_timeout = 20
+
+  cache_settings = {
+    max_files = 800
+    max_space = 300
+  }
+
+  file_settings = {
+    allow_purge   = false
+    file_threshold = "WARN"
+    max_file_size = 200000
+    max_old_files = 10
+  }
+
+  duplicate_settings = {
+    suppress_interval  = 10
+    suppress_threshold = 5
+  }
+}
+`,
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttrSet("ciphertrust_cte_profile.profile", "id"),
+				),
+			},
+		},
+		// Delete testing automatically occurs in TestCase
+	})
+}
--- sample-scripts/cte/profile/main.tf
+++ sample-scripts/cte/profile/main.tf
@@ -18,38 +18,53 @@ resource "ciphertrust_cte_profile" "profile" {
   name        = "TEST_API_Profile1"
   description = "Testing profile using Terraforms"
 
-  client_logging_configuration {
-    threshold      = "ERROR"
-    duplicates     = "ALLOW"
-    syslog_enabled = false
-    file_enabled   = false
-    upload_enabled = false
+  cache_settings =  {
+    max_space = 500
+    max_files = 250
   }
 
-  cache_settings {
-    max_space = 100
-    max_files = 205
+
+  concise_logging = true
+
+  duplicate_settings = {
+    suppress_threshold = 20
+    suppress_interval  = 500
   }
 
-  syslog_settings {
-    local = false
-    servers {
+  file_settings = {
+    allow_purge    = true
+    max_old_files  = 10
+    max_file_size  = 2000000
+    file_threshold = "ERROR"
+  }
+
+  client_logging_configuration = {
+   threshold = "ERROR",
+		duplicates = "SUPPRESS",
+		syslog_enabled = true,
+		file_enabled = true,
+		upload_enabled = true
+  }
+
+  syslog_settings = {
+    local = true
+    servers = [{
       name           = "localhost"
       port           = 22
       protocol       = "TCP"
       message_format = "LEEF"
-    }
+    }]
     syslog_threshold = "ERROR"
   }
 
-  file_settings {
-    allow_purge    = false
-    max_old_files  = 10
-    max_file_size  = 1000000
-    file_threshold = "ERROR"
-  }
-  duplicate_settings {
-    suppress_threshold = 5
-    suppress_interval  = 600
-  }
+	upload_settings =  {
+		min_interval = 10
+		max_interval = 20
+		max_messages = 2000
+		connection_timeout = 50
+		job_completion_timeout = 600
+		drop_if_busy = true
+		upload_threshold = "ERROR"
+	}
+
 }
