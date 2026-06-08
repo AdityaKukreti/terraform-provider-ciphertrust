ab51bd9f92d48b9bc7408aab02f80c0f96ed15c7
Aditya Kukreti <74974564+AdityaKukreti@users.noreply.github.com>
2026-06-09 02:45:36 +0530

    Make tiny issue bot script complete

--- terraform-bot/bot.py
+++ terraform-bot/bot.py
@@ -1,8 +1,6 @@
 import json,os,subprocess as s
-e=json.load(open(os.environ['GITHUB_EVENT_PATH']));i=e.get('issue')or{}
-t=(i.get('title','')+' '+str(i.get('body'))).lower()
-L=[]
-if any(x in t for x in'bug error fail crash'.split()):L+=['bug']
-if any(x in t for x in'feature enhance improve'.split()):L+=['enhancement']
-if any(x in t for x in'docs doc readme'.split()):L+=['documentation']
-if L:s.run(['gh','issue','edit',str(i['number']),'--add
+e=json.load(open(os.environ['GITHUB_EVENT_PATH']))
+i=e['issue'];t=(i['title']+' '+str(i.get('body'))).lower();L=[]
+if 'bug' in t or 'error' in t:L+=['bug']
+if 'docs' in t or 'readme' in t:L+=['documentation']
+if L:s.run(['gh','issue','edit',str(i['number']),'--add-label',','.join(L)])
