861ebf1b7875594bd9e7fee5496f447e377550e9
Aditya Kukreti <74974564+AdityaKukreti@users.noreply.github.com>
2026-06-09 03:19:28 +0530

    Run duplicate detector on issue events

--- terraform-bot/bot.py
+++ terraform-bot/bot.py
@@ -1,6 +1,6 @@
 import json,os,sys
 sys.path.append(os.path.dirname(__file__))
-import labeler,commands
+import labeler,commands,duplicates
 
 e=json.load(open(os.environ['GITHUB_EVENT_PATH']))
 if 'issue' not in e:raise SystemExit(0)
@@ -9,3 +9,4 @@ if 'comment' in e:
     commands.run(issue,e['comment'])
 else:
     labeler.run(issue)
+    if e.get('action')=='opened':duplicates.run(issue)
