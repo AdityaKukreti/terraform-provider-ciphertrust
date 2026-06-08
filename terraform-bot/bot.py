34bf28e506038b38be2c83b6a8d598ad198f64cc
Aditya Kukreti <74974564+AdityaKukreti@users.noreply.github.com>
2026-06-09 03:13:42 +0530

    Pass full comment event to command handler

--- terraform-bot/bot.py
+++ terraform-bot/bot.py
@@ -6,6 +6,6 @@ e=json.load(open(os.environ['GITHUB_EVENT_PATH']))
 if 'issue' not in e:raise SystemExit(0)
 issue=e['issue']
 if 'comment' in e:
-    commands.run(issue,e['comment'].get('body','').strip())
+    commands.run(issue,e['comment'])
 else:
     labeler.run(issue)
